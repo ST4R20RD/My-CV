a45630208c4f6bcbf6cab576611daf16caf99a8d
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-18 16:42:52 +0000

    Routes configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
