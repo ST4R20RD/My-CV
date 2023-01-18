7cf751840a20372bcae4f4fee71a846448229dc1
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-18 17:14:40 +0000

    added Header and Footer components

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { Footer } from "../Footer";
+import { Header } from "../Header";
 
 export function Layout() {
   return (
     <div>
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
