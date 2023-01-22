812dd6f22f4cd8a0825e49f823e9bcc81bd763cd
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-22 20:05:20 +0000

    Added Home and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components";
+import { Home } from "./pages";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route />
+        <Route index element={<Home />} />
       </Route>
     </Routes>
   );
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,43 @@
+import styled from "styled-components";
+import { FiGithub } from "react-icons/fi";
+import { FaLinkedinIn } from "react-icons/fa";
+
 export function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Link>
+          <FiGithub />
+        </Link>
+        <Link>
+          <FaLinkedinIn />
+        </Link>
+      </Wrapper>
+    </Container>
+  );
 }
+
+const Container = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  margin: 0 1rem;
+  padding: 10px;
+  border: solid 1px;
+  border-radius: 10%;
+  background: #fff;
+  svg {
+    color: black;
+  }
+`;
