05e44c22c22d46dc9b5a43e21e08650036387c4f
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-25 15:45:12 +0000

    Added Title & Fixed some CSS on About Page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -9,10 +9,10 @@ export function About() {
         <Skills />
       </Box>
       <TextBox>
-        I am a highly skilled front-end developer with expertise in full stack development using the
-        MERN stack (MongoDB, Express, React, NodeJS) and a passion for UX/UI design. I have
-        completed a Full Stack Developer Bootcamp with Ironhack, which has given me a solid
-        understanding of web development best practices and the ability to create dynamic and
+        <h1>About Me</h1>I am a highly skilled front-end developer with expertise in full stack
+        development using the MERN stack (MongoDB, Express, React, NodeJS) and a passion for UX/UI
+        design. I have completed a Full Stack Developer Bootcamp with Ironhack, which has given me a
+        solid understanding of web development best practices and the ability to create dynamic and
         responsive user interfaces. My proficiency in HTML, CSS, and JavaScript allows me to write
         clean, efficient code that meets the latest web standards. I am well-versed in the latest
         web development technologies and have a solid understanding of the entire web development
@@ -29,8 +29,9 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin: 3rem 0;
   & > * {
-    margin: 2rem;
+    margin: 1rem;
   }
   @media (min-width: 940px) {
     height: 100%;
@@ -43,6 +44,9 @@ const Box = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  & > * {
+    margin: 20px 0;
+  }
 `;
 
 const ProfilePic = styled.img`
@@ -53,4 +57,5 @@ const ProfilePic = styled.img`
 
 const TextBox = styled.p`
   max-width: 500px;
+  margin-bottom: 10rem;
 `;
